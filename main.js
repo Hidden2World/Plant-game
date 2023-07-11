3e83742ce71eeafe6bedfd51180b91b4044bf3d7
Hidden2World <101087222+Hidden2World@users.noreply.github.com>
2023-07-11 16:26:14 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,41 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const gridsize = 10; 
+    const gridsize = 10;
     const grid = document.getElementById("grid");
     const cells = [];
     let score = 2;
 
+    const plantOptions = [
+        { name: "Rose", color: "red" },
+        { name: "Sunflower", color: "yellow" },
+        { name: "Tulip", color: "pink" },
+        { name: "Lily", color: "orange" },
+        { name: "Orchid", color: "purple" },
+        { name: "Daisy", color: "white" },
+        { name: "Bluebell", color: "blue" }
+    ];
+
+    const container = document.createElement("div");
+    container.classList.add("container");
+
+    // Create the plant selection dropdown
     
+    const dropdownContainer = document.createElement("div");
+    dropdownContainer.classList.add("dropdown-container");
+    const dropdownLabel = document.createElement("label");
+    dropdownLabel.textContent = "Select a plant: ";
+    const plantSelect = document.createElement("select");
+    plantOptions.forEach(function(plant) {
+        const option = document.createElement("option");
+        option.value = plant.color;
+        option.textContent = plant.name;
+        plantSelect.appendChild(option);
+        
+        
+    });
+    dropdownContainer.appendChild(dropdownLabel);
+    dropdownContainer.appendChild(plantSelect);
+    container.appendChild(dropdownContainer);
+
     // Create the grid
     for (let i = 0; i < gridsize; i++) {
         for (let j = 0; j < gridsize; j++) {
@@ -14,20 +45,21 @@ document.addEventListener("DOMContentLoaded", function() {
             cells.push(cell);
         }
     }
+    container.appendChild(grid);
+    document.body.appendChild(container);
 
     // Plant a plant when a cell is clicked
     cells.forEach(function(cell) {
-        
         cell.addEventListener("click", function() {
-            
             if (!cell.classList.contains("planted") && score >= 1) {
+                const selectedColor = plantSelect.value;
                 cell.classList.add("planted");
-                cell.style.backgroundColor = getRandomColor();
+                cell.style.backgroundColor = selectedColor;
                 score -= 1;
                 updateScore();
                 plantGrowthAnimation(cell);
             } else if (cell.classList.contains("fully-grown")) {
-                score += 1;
+                
                 resetPlant(cell);
             }
         });
@@ -51,11 +83,10 @@ document.addEventListener("DOMContentLoaded", function() {
         cell.style.transformOrigin = "center";
         cell.style.transform = `translate(${initialX}px, ${initialY}px)`;
         cell.classList.add("initial");
-        
 
         const growthInterval = setInterval(function() {
             const currentSize = parseInt(cell.style.width) || initialSize;
-            
+
             if (currentSize < finalSize) {
                 const newSize = currentSize + 5;
                 const newX = centerX - newSize / 2;
@@ -72,30 +103,21 @@ document.addEventListener("DOMContentLoaded", function() {
                 score += 1;
                 updateScore();
             }
-        }, 600000);
-    }
-
-    // Function to generate a random flower color
-    function getRandomColor() {
-        const colors = ["red", "blue", "yellow", "pink", "orange", "purple", "green"];
-        return colors[Math.floor(Math.random() * colors.length)];
+        }, 30000);
     }
 
     // Function to update the score
     function updateScore() {
         const scoreDisplay = document.getElementById("score");
         scoreDisplay.textContent = `Plants: ${score}`;
-        
     }
 
     // Function to reset the plant
     function resetPlant(cell) {
         cell.classList.remove("fully-grown");
         cell.classList.remove("planted");
-        cell.style.width = "10";
-        cell.style.height = "10";
-        cell.style.borderRadius = "50%";
-        cell.style.transform = `translate(${newX}px, ${newY}px)`;
+        cell.style.backgroundColor = "";
         score += 1;
+        updateScore();
     }
 });
